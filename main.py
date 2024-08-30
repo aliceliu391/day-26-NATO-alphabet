72de5d035dc17f10b6ce089ac741e0adc9b90c35
aliceliu391 <112590839+aliceliu391@users.noreply.github.com>
2024-08-29 22:53:25 -0400

    Update main.py to use Exception Handling

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ import pandas
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 alphabet_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 
-word = input("Enter a word: ").strip().upper()
-word_list = [alphabet_dict[letter] for letter in word]
-print(word_list)
+not_complete = True
+while not_complete:
+    word = input("Enter a word: ").strip().upper()
+    try:
+        word_list = [alphabet_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only numbers in the alphabet!")
+    else:
+        print(word_list)
+        not_complete = False
